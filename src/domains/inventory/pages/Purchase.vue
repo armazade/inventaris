6eb49ecbebc0ab584a657a6ee5f754f58afd3dee
armazade <armantje2001@gmail.com>
2025-11-14 13:21:11 +0100

    Remove header from InventoryTable and add Purchase page with product filtering

---
+++ src/domains/inventory/pages/Purchase.vue
@@ -0,0 +1,16 @@
+<script setup>
+import {computed} from 'vue';
+import InventoryTable from '../../../components/tables/InventoryTable.vue';
+import {getProducts} from '../store.ts';
+
+const products = computed(() => getProducts.value.filter(product => product.actualQuantity < product.minimumQuantity));
+</script>
+
+<template>
+    <h1>Purchase</h1>
+    <div class="container">
+        <InventoryTable :products="products" />
+    </div>
+</template>
+
+<style scoped></style>
