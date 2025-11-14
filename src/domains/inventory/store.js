d683aecd68f538414ef4b5c665e28024069aa1e4
armazade <armantje2001@gmail.com>
2025-11-14 09:42:33 +0100

    Add initial inventory data to store

---
+++ src/domains/inventory/store.js
@@ -0,0 +1,9 @@
+export const inventory = [
+    {id: 1, name: 'Item A', actualQuantity: 10, minimumQuantity: 5},
+    {id: 2, name: 'Item B', actualQuantity: 20, minimumQuantity: 10},
+    {id: 3, name: 'Item C', actualQuantity: 15, minimumQuantity: 7},
+    {id: 4, name: 'Item D', actualQuantity: 30, minimumQuantity: 15},
+    {id: 5, name: 'Item E', actualQuantity: 25, minimumQuantity: 12},
+    {id: 6, name: 'Item F', actualQuantity: 5, minimumQuantity: 3},
+    {id: 7, name: 'Item G', actualQuantity: 12, minimumQuantity: 6},
+];
