1c6ac1af549672e703c2471574fab710a842bb6b
armazade <armantje2001@gmail.com>
2025-11-14 11:14:57 +0100

    Add InventoryTable component and integrate it into Index page

---
+++ src/components/tables/InventoryTable.vue
@@ -0,0 +1,41 @@
+<script setup>
+const props = defineProps({
+    products: {
+        type: Array,
+        required: true,
+    },
+});
+
+const emit = defineEmits(['delete']);
+
+const onDelete = id => {
+    emit('delete', id);
+};
+</script>
+
+<template>
+    <div class="container">
+        <h1>Inventory</h1>
+
+        <table>
+            <thead>
+                <tr>
+                    <th>Product</th>
+                    <th>Actual Quantity</th>
+                    <th>Minimum Quantity</th>
+                    <th>Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="product in products" :key="product.name">
+                    <td>{{ product.name }}</td>
+                    <td>{{ product.actualQuantity }}</td>
+                    <td>{{ product.minimumQuantity }}</td>
+                    <td><button @click="onDelete(product.id)">Delete</button></td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+
+<style scoped></style>
--- src/domains/inventory/pages/Index.vue
+++ src/domains/inventory/pages/Index.vue
@@ -1,6 +1,18 @@
-<script setup></script>
+<script setup>
+import InventoryTable from '../../../components/tables/InventoryTable.vue';
+import {getProducts, deleteProduct} from '../store.ts';
 
-<template></template>
+const products = getProducts;
+
+const handleDelete = id => {
+    deleteProduct(id);
+};
+</script>
+
+<template>
+    <div class="container">
+        <InventoryTable :products="products" @delete="handleDelete" />
+    </div>
+</template>
 
 <style scoped></style>
-
