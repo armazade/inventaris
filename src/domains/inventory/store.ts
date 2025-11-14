a5a449ae2ed9f4f2f7826aab0ddcaee2d52e9ba6
armazade <armantje2001@gmail.com>
2025-11-14 11:14:41 +0100

    refatcor to ts

---
+++ src/domains/inventory/store.ts
@@ -0,0 +1,34 @@
+import {computed, ref, type Ref} from 'vue';
+
+// Types
+export interface InventoryProduct {
+    id: number;
+    name: string;
+    actualQuantity: number;
+    minimumQuantity: number;
+}
+
+// State
+export const products: Ref<InventoryProduct[]> = ref([
+    {id: 1, name: 'Item A', actualQuantity: 10, minimumQuantity: 5},
+    {id: 2, name: 'Item B', actualQuantity: 20, minimumQuantity: 10},
+    {id: 3, name: 'Item C', actualQuantity: 15, minimumQuantity: 7},
+    {id: 4, name: 'Item D', actualQuantity: 30, minimumQuantity: 15},
+    {id: 5, name: 'Item E', actualQuantity: 25, minimumQuantity: 12},
+    {id: 6, name: 'Item F', actualQuantity: 5, minimumQuantity: 3},
+    {id: 7, name: 'Item G', actualQuantity: 12, minimumQuantity: 6},
+]);
+
+// Getters
+export const getProducts = computed<InventoryProduct[]>(() => products.value);
+export const getProductById = (id: number) =>
+    computed<InventoryProduct | undefined>(() => products.value.find(product => product.id === id));
+
+// Actions
+
+export const deleteProduct = (id: number): void => {
+    const index = products.value.findIndex(product => product.id == id);
+    if (index !== -1) {
+        products.value.splice(index, 1);
+    }
+};
